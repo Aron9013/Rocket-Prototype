31fbb7c72141c41115abff7f75978dd30421c835
Aron9013 <aron.d28@gmail.com>
2018-09-27 22:20:06 +0100

    added animation for launching rocket

--- rocket-animation-test/src/app/app.component.ts
+++ rocket-animation-test/src/app/app.component.ts
@@ -6,4 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public launchRocket: boolean = false;
+
+  public blastoff(): void {
+    this.launchRocket = true;
+  }
 }
